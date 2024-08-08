8214218a186c06441cc0b072d62a115b68f2c761
Chinmay-Mohite-23 <mohite.chinmay.231102@gmail.com>
2024-08-08 23:55:57 +0530

    Setting up OpenAI embeddings function

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,11 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 import { downloadFromS3 } from "./s3-server";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import {
+  Document,
+  RecursiveCharacterTextSplitter,
+} from "@pinecone-database/doc-splitter";
+import { TextEncoder } from "util";
 
 let pinecone: Pinecone | null = null;
 
@@ -13,10 +18,10 @@ export const getPineconeClient = () => {
 
 type PDFPage = {
   pageContent: string;
-  metadata : {
-    loc : {pageNumber : number}
-  }
-}
+  metadata: {
+    loc: { pageNumber: number };
+  };
+};
 
 export async function loadS3intoPinecone(file_key: string) {
   // Loading the PDF from S3 into Pinecone ::
@@ -28,7 +33,34 @@ export async function loadS3intoPinecone(file_key: string) {
   const loader = new PDFLoader(file_name);
   const pages = (await loader.load()) as PDFPage[];
 
-  
+  // Splitting the file into chunks ::
+  // return pages;
+  const documents = await Promise.all(pages.map(prepareDocuments));
 
-  return pages;
+  // Vectorize and Embed the documents ::
+
+}
+
+//async function embedDocument
+
+export const truncateStringByBytes = (str: string, bytes: number) => {
+  const enc = new TextEncoder();
+  return new TextDecoder("utf-8 ").decode(enc.encode(str).slice(0, bytes));
+};
+
+async function prepareDocuments(page: PDFPage) {
+  let { pageContent, metadata } = page;
+  pageContent = pageContent.replace(/\n/g, "");
+  //Doc Splitting ::
+  const splitter = new RecursiveCharacterTextSplitter();
+  const docs = await splitter.splitDocuments([
+    new Document({
+      pageContent,
+      metadata: {
+        pageNumber: metadata.loc.pageNumber,
+        text: truncateStringByBytes(pageContent, 36000),
+      },
+    }),
+  ]);
+  return docs;
 }
